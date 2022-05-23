6860a2599cd94fde03c4874ab4a86c4a1c897d72
Joel Mansilla <mansilla.n.joel@gmail.com>
2022-05-23 15:26:17 -0300

    Desafio: Clase 2

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+
+  return <header> 
+    <h1> E-Commerce </h1>
+
+    <nav>
+   	  <p> Inicio </p>
+      <p> Productos </p>
+      <p> Servicios </p>
+      <p> Nosotros </p>
+      <p> Contacto </p>
+    </nav>
+  </header>
+}
