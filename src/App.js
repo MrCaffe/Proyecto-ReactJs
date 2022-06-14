4b157c6d9eb2bbd3dac798ce5bc1ab273e7b90b6
Joel Mansilla <mansilla.n.joel@gmail.com>
2022-06-14 13:05:13 -0300

    Desafio: Clase 6(ReEntrega) Fixed Missing File App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
+// Tools
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar} from './components/Navbar/Navbar.js';
-import {ItemListContainer} from './components/ItemListContainer/ItemListContainer.js';
-import {ItemCount} from './components/ItemCount/ItemCount.js';
-import {PokeApi} from './components/Ejemplos/PokeApi/PokeApi.js'
 
+// Components
+import {Navbar} from './components/Navbar/Navbar.js';
+import {Footer} from './components/Footer/Footer.js';
+import {ItemListContainer} from './components/ItemListContainer/ItemListContainer.js';
+import {ItemDetailContainer} from './components/ItemDetailContainer/ItemDetailContainer.js';
+import {ItemCount} from './components/ItemCount/ItemCount.js';
 
 
 function App() {
 
-  // const producto = {
-  //   id: '00001',
-  //   rol: 'gaming'
-  // }
-
-  // const servicio = {
-  //   id: 'aaaaa',
-  //   rol: 'internet',
-  // }
-
-  const product_blocks = [7796569263521, "Blocks Emblocados", "A4", 40, "Cuadriculadas"]
-  const product_tapa_carton = [77965694322224, "Cuaderno 123 Tapa Dura", "Oficio", 100, "Rayadas"]
 
   return (
-    <div>
-
+    <section>
       <Navbar/>
-
       <ItemListContainer/>
-     {/* <PokeApi/>*/}
+      {/*<ItemDetailContainer/>*/}
+      <ItemCount/>
 
-    </div>
+      <Footer/>
+
+
+    </section>
   );
 }
 
 export default App;
+
